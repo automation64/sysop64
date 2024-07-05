b51a2b6269eccfc79196b15f2020e4b406f9e5e3
SerDigital64 <serdigital64@gmail.com>
2024-07-05 09:32:33 +0000

    test: fix lint

--- docs/snippets/task/import.bash
+++ docs/snippets/task/import.bash
@@ -2,8 +2,8 @@
 declare SYSOP64_CORE_PATH_ROOT="${SYSOP64_CORE_PATH_ROOT:-/opt/sysop64}"
 # shellcheck source-path=SCRIPTDIR/..
 source "${SYSOP64_CORE_PATH_ROOT}/core/core-lib" || { echo "Error: unable to load module library" && exit 1; }
-# shellcheck source-path=SCRIPTDIR/../../lib/bl64
+# shellcheck disable=SC2015 source-path=SCRIPTDIR/../../lib/bl64
 source "${SYSOP64_CORE_PATH_BL64}/bashlib64-core.bash" ||
   { echo "Error: unable to load bashlib64" && exit 1; }
-# shellcheck source-path=SCRIPT_PATH/..
+# shellcheck source-path=SCRIPTDIR/..
 source "${SYSOP64_CORE_PATH_ROOT}/X_MODULE_SHARED_X/X_MODULE_SHARED_X-lib" || { echo "Error: unable to load module library" && exit 1; }
