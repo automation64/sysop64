8e4550310db0cfa628ed9f07aab863512783f512
SerDigital64 (Max) <serdigital64@gmail.com>
2024-08-25 20:34:12 +0200

    feat: add modules (#14)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,22 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.10.0]
+
+### Added
+
+- Linux
+  - Added verbosity control
+- Restic
+  - Added verbosity control
+- RSync
+  - Added verbosity control
+- Module
+  - ArgoCD
+  - Kind
+  - OpenSSH
+  - GitHub-User
+
 ## [0.9.0]
 
 ### Added
@@ -20,7 +36,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Core-Lib
-  - Global flags: SYSOP64_CORE_SET_DEBUG, SYSOP64_CORE_SET_VERBOSE
+  - Global flags: S64_CORE_SET_DEBUG, S64_CORE_SET_VERBOSE
 - Linux-Lib
   - Added Fedora support
 - Linux-Distro
---
+++ docs/snippets/module-cli/cli_setup.bash
@@ -0,0 +1,23 @@
+# template-module-setup: 1.1.0
+  # X_GLOBALS_PLACEHOLDER_X
+  declare S64_X_MODULE_CAPS_X_PATH_CLI="${S64_X_MODULE_CAPS_X_PATH_CLI:-}"
+
+# X_SETUP_PLACEHOLDER_X
+  local command='X_MODULE_COMMAND_CLI_X'
+  if [[ -z "$S64_X_MODULE_CAPS_X_PATH_CLI" ]]; then
+    if [[ -x "/home/linuxbrew/.linuxbrew/bin/${command}" ]]; then
+      S64_X_MODULE_CAPS_X_PATH_CLI="/home/linuxbrew/.linuxbrew/bin/${command}"
+    elif [[ -x "/opt/homebrew/bin/${command}" ]]; then
+      S64_X_MODULE_CAPS_X_PATH_CLI="/opt/homebrew/bin/${command}"
+    elif [[ -x "/usr/local/bin/${command}" ]]; then
+      S64_X_MODULE_CAPS_X_PATH_CLI="/usr/local/bin/${command}"
+    elif [[ -x "/usr/bin/${command}" ]]; then
+      S64_X_MODULE_CAPS_X_PATH_CLI="/usr/bin/${command}"
+    else
+      bl64_check_alert_resource_not_found "$command"
+      return $?
+    fi
+  else
+    bl64_check_command "$S64_X_MODULE_CAPS_X_PATH_CLI" ||
+      return $?
+  fi
