ec5802ca82fe87b61543f9572d12819f44dcc078
SerDigital64 <serdigital64@gmail.com>
2025-01-17 06:28:33 +0000

    doc: upgrade templates

--- docs/snippets/task-import/import.bash
+++ docs/snippets/task-import/import.bash
@@ -1,8 +1,7 @@
-# template-import: 2.0.0
+# template-import: 2.0.1
 # X_IMPORTS_PLACEHOLDER_X
 declare S64_CORE_PATH_ROOT="${S64_CORE_PATH_ROOT:-/opt/sysop64}"
 # shellcheck source-path=SCRIPTDIR/..
-source "${S64_CORE_PATH_ROOT}/core/core-lib" &&
-  source "${S64_CORE_PATH_ROOT}/core/X_MODULE_X-lib" || { echo "Error: unable to load module library" && exit 1; }
+source "${S64_CORE_PATH_ROOT}/core/core-lib" && source "${S64_CORE_PATH_ROOT}/core/X_MODULE_X-lib" || { echo "Error: unable to load module library" && exit 1; }
 # shellcheck disable=SC2015 source-path=SCRIPTDIR/../../lib/bl64
 source "${S64_CORE_PATH_BL64}/bashlib64-core.bash" || { echo 'Error: unable to load bashlib64' && exit 1; }
