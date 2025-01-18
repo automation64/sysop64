3f6227373f006f869c897e878de688bb43ddd203
SerDigital64 <serdigital64@gmail.com>
2025-01-18 07:32:36 +0000

    doc: update skel

--- docs/snippets/module-cli/cli_run.bash
+++ docs/snippets/module-cli/cli_run.bash
@@ -1,6 +1,22 @@
-# X_FUNCTIONS_PLACEHOLDER_X
 # template-module-cli: 1.1.0
-function s64_X_MODULE_X_lib_run_X_MODULE_CLI_NAME_X() {
+
+# X_IMPORTS_PLACEHOLDER_X
+# shellcheck disable=SC2015 source-path=SCRIPTDIR/../../lib/bl64
+source "${S64_CORE_PATH_BL64}/bashlib64-module-bsh.bash" ||
+  { echo 'Error: unable to load bashlib64' && exit 1; }
+
+# X_GLOBALS_PLACEHOLDER_X
+  declare S64_X_MODULE_CAPS_X_PATH_CLI="${S64_X_MODULE_CAPS_X_PATH_CLI:-}"
+
+# X_CLI_SETUP_PLACEHOLDER_X
+  if [[ -z "$S64_X_MODULE_CAPS_X_PATH_CLI" ]]; then
+    S64_X_MODULE_CAPS_X_PATH_CLI="$(bl64_bsh_command_locate 'X_CLI_COMMAND_X')"
+  else
+    bl64_check_command "$S64_X_MODULE_CAPS_X_PATH_CLI"
+  fi
+
+# X_FUNCTIONS_PLACEHOLDER_X
+function s64_X_MODULE_X_lib_run_X_CLI_COMMAND_X() {
   bl64_dbg_app_show_function "$@"
   local verbose_flag='--verbose'
   bl64_msg_app_verbose_enabled || verbose_flag=' '
