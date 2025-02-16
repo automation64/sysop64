37b078d39aa3c7ef9d31cdaa0b3818e24f65ef64
X_GIT_USER_NAME_X <X_GIT_USER_EMAIL_X>
2025-02-16 10:03:35 +0000

    refact: upgrade bl64

--- docs/snippets/module-cli/cli_run.bash
+++ docs/snippets/module-cli/cli_run.bash
@@ -19,7 +19,7 @@ source "${S64_CORE_PATH_BL64}/bashlib64-module-bsh.bash" ||
 function s64_X_MODULE_X_lib_run_X_CLI_COMMAND_X() {
   bl64_dbg_app_show_function "$@"
   local verbose_flag='--verbose'
-  bl64_msg_app_verbose_enabled || verbose_flag=' '
+  bl64_msg_app_verbose_is_enabled || verbose_flag=' '
   bl64_dbg_app_trace_start
   # shellcheck disable=SC2086
   "$S64_X_MODULE_CAPS_X_PATH_CLI" \
