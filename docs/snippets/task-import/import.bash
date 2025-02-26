a69e39a59f79757cf079ad0c4f9121872265ca81
SerDigital64 (Max) <serdigital64@gmail.com>
2025-02-26 07:40:32 +0100

    rel: promote 1.1.0 (#24)

--- docs/snippets/task-import/import.bash
+++ docs/snippets/task-import/import.bash
@@ -1,6 +1,8 @@
 # template-import: 2.0.1
 # X_IMPORTS_PLACEHOLDER_X
-declare S64_CORE_PATH_ROOT="${S64_CORE_PATH_ROOT:-/opt/sysop64}"
+declare S64_CORE_PATH_AT64="${HOME}/at64"
+[[ ! -d "$S64_CORE_PATH_AT64" ]] && S64_CORE_PATH_AT64='/opt'
+declare S64_CORE_PATH_ROOT="${S64_CORE_PATH_ROOT:-${S64_CORE_PATH_AT64}/sysop64}"
 # shellcheck source-path=SCRIPTDIR/..
 source "${S64_CORE_PATH_ROOT}/core/core-lib" && source "${S64_CORE_PATH_ROOT}/core/X_MODULE_X-lib" || { echo "Error: unable to load module library" && exit 1; }
 # shellcheck disable=SC2015 source-path=SCRIPTDIR/../../lib/bl64
