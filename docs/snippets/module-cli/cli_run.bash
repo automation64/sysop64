3b718fe1d51cd449074f788e8d9f62a041de1b58
bl64_vcs_run_git <nouser@nodomain>
2025-11-17 17:08:31 +0100

    real: promote 2.9.0

--- docs/snippets/module-cli/cli_run.bash
+++ docs/snippets/module-cli/cli_run.bash
@@ -1,7 +1,7 @@
 # template-module-cli: 1.2.0
 
 # X_IMPORTS_PLACEHOLDER_X
-# shellcheck disable=SC2015 source-path=SCRIPTDIR/../../lib/bl64
+# shellcheck disable=SC2015 source-path=/opt/bl64
 source "${S64_CORE_PATH_BL64}/bashlib64-module-bsh.bash" ||
   { echo 'Error: unable to load bashlib64' && exit 1; }
 
