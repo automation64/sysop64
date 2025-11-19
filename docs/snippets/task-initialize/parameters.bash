5acd5ad4ecaf8fdcb13550569eb96301e9dbc97e
bl64_vcs_run_git <nouser@nodomain>
2025-11-19 19:22:13 +0100

    real: promote 2.9.1

--- docs/snippets/task-initialize/parameters.bash
+++ docs/snippets/task-initialize/parameters.bash
@@ -1,4 +1,3 @@
   # X_INIT_PARAMETER_PLACEHOLDER_X
-  [[ -n "$MY_OPTION" && -n "$MY_COMMAND" ]] &&
-    bl64_check_parameter 'MY_PARAMETERX' ||
+  [[ -n "$MY_OPTION" && -n "$MY_COMMAND" ]] ||
     { bl64_msg_help_show && return 1; }
