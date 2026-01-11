7c8f93de372dfa0a94472d2209d94f4e656b4085
SerDigital64 (Max) <serdigital64@gmail.com>
2026-01-11 09:08:44 +0100

    real: promote 3.2.0 (#58)

--- docs/snippets/module-cli/cli_run.bash
+++ docs/snippets/module-cli/cli_run.bash
@@ -3,7 +3,7 @@
 # X_IMPORTS_PLACEHOLDER_X
 # shellcheck disable=SC2015 source-path=/opt/bl64
 source "${S64_CORE_PATH_BL64}/bashlib64-module-bsh.bash" ||
-  { echo 'Error: unable to load bashlib64' && exit 1; }
+  { s64_core_lib_check_bl64_load && exit 1; }
 
 # X_GLOBALS_PLACEHOLDER_X
   declare S64_X_MODULE_CAPS_X_PATH_CLI="${S64_X_MODULE_CAPS_X_PATH_CLI:-}"
