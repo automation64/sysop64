bc502fd17414acd3308f90aa4383ea410c3923bf
bl64_vcs_run_git <nouser@nodomain>
2026-01-25 18:20:12 +0100

    real: promote 3.3.1

--- docs/snippets/module-cli/cli_run.bash
+++ docs/snippets/module-cli/cli_run.bash
@@ -1,7 +1,6 @@
 # template-module-cli: 1.2.0
 
 # X_IMPORTS_PLACEHOLDER_X
-# shellcheck disable=SC2015 source-path=/opt/bl64
 source "${S64_CORE_PATH_BL64}/bashlib64-module-bsh.bash" ||
   { s64_core_lib_check_bl64_load && exit 1; }
 
